2fc162ce54fce1f1be7a53bc1a9e7adafaa57bb4
TanyEm <tanyem@protonmail.com>
2021-03-03 14:16:04 +0200

    Part 2. Tasks 1-4

---
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hi");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("hi");
+        int second = Integer.valueOf(scanner.nextLine());
+        double sqrt = Math.sqrt(first+second);
+        System.out.println(sqrt);
+    }
+}
