f27dd2043a4e646b50c381988dba5d63db207c61
TanyEm <tanyem@protonmail.com>
2021-03-21 12:27:57 +0200

    Part 10. Tasks 5-10

---
+++ Part 10/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintingUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList <String> strings = new ArrayList<>();
+        
+        while(true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            strings.add(input);
+        }
+        
+        strings.stream()
+                .sorted()
+                .forEach(str -> System.out.println(str));
+    }
+}
